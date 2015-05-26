110945348dda95349cec27d093246009af615d67
shsong97 <shsong97@gmail.com>
2015-05-26 21:26:26 +0900

    white noise install

--- stockdiary/settings.py
+++ stockdiary/settings.py
@@ -113,6 +113,8 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
