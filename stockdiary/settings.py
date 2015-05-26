00bc857f692376bbbe05f9791046d3336b7e837c
shsong97 <shsong97@gmail.com>
2015-05-26 20:32:56 +0900

    setting update

--- stockdiary/settings.py
+++ stockdiary/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'wpqh8$7o3rk#6aarf$()scxcu%!4*lrhj6lj8#vn(gbx3ktj8v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
