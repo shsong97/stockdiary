67154caee3b63cc9c0d6c4344014660c9ee71825
shsong97 <shsong97@gmail.com>
2015-05-27 22:22:16 +0900

    update

--- stockdiary/wsgi.py
+++ stockdiary/wsgi.py
@@ -14,6 +14,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stockdiary.settings")
 
-application = get_wsgi_application()
+# application = get_wsgi_application()
 from dj_static import Cling
 application = Cling(get_wsgi_application())
