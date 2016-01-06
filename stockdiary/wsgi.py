a2795c437af98f06338809d229c461bc723dd169
shsong97 <shsong97@gmail.com>
2016-01-06 14:55:09 +0000

    wsgi fixed - whitenoise setting

--- stockdiary/wsgi.py
+++ stockdiary/wsgi.py
@@ -15,5 +15,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stockdiary.settings")
 
 application = get_wsgi_application()
-# from dj_static import Cling
-# application = Cling(get_wsgi_application())
+
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
